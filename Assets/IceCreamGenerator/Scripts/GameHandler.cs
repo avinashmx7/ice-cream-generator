1b81f3ae0541083b5f58fe5c3dd21a8f292b7b4b
Avinash <avinashmx7@gmail.com>
2022-06-15 20:19:57 +0530

    Added cream functionality using beizer curve

--- Assets/IceCreamGenerator/Scripts/GameHandler.cs
+++ Assets/IceCreamGenerator/Scripts/GameHandler.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using PathCreation;
 using UnityEngine;
+using UnityEngine.ProBuilder;
+using UnityEngine.UI;
 
-public class GameHandler : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class GameHandler : MonoBehaviour {
+    [SerializeField] private GameObject cylinderPrefab;
+    [SerializeField] private PathCreator pathCreator;
+    private Button _dummyButton;
+    private Queue<CustomPathFollower> _cylinderPrefabs;
+    private bool _generateCream = false, isGameCompleted = false;
+    private Color _currentSelectColor = Color.red;
+
+    private void Start() {
+        _cylinderPrefabs = new Queue<CustomPathFollower>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        if (!Input.GetMouseButton(0) || isGameCompleted || !_generateCream) return;
+        var cylinder = Instantiate(cylinderPrefab, transform);
+        cylinder.transform.GetChild(0).GetComponent<MeshRenderer>().materials[0].color = _currentSelectColor;
+        var customPathCreator = cylinder.GetComponent<CustomPathFollower>();
+        customPathCreator.PathCreator = pathCreator;
+        _cylinderPrefabs.Enqueue(customPathCreator);
+
+        foreach (var pathFollower in _cylinderPrefabs) {
+            pathFollower.ShiftPosition();
+        }
+
+        if (_cylinderPrefabs.TryPeek(out var customPath)) {
+            isGameCompleted = customPath.IsPathCompleted();
+        }
     }
+
+
+    public void SetColor(string colorName) {
+        _currentSelectColor = colorName switch {
+            "Red" => Color.red,
+            "Blue" => Color.blue,
+            "Green" => Color.green,
+            _ => Color.red
+        };
+        _generateCream = true;
+    }
+    
+    public void StopCreamGenerate() {
+        _generateCream = false;
+    }
+
 }
